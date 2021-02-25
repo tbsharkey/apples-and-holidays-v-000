7d8731425c82533bf55a8a90a9996e36346bd4e3
Tom Sharkey <tbsharkey@gmail.com>
2021-02-25 17:05:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,6 +79,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
       holidays.each do |holiday, items|
       holiday  if items == "BBQ"
-end          
+end
 end
 end
