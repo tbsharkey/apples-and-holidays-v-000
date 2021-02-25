00ef53504b75c622fc5f1e2588805af13f28da99
Tom Sharkey <tbsharkey@gmail.com>
2021-02-25 16:23:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ end
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   def add_supply_to_winter_holiday(supplies_hash, supply)
-    supplies_hash[:winter][:new_years] << supply
+    supplies_hash[:winter] << supply
   end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
