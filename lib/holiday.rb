09d337691bb9ae468251b214c22bd9d18d9089d8
Tom Sharkey <tbsharkey@gmail.com>
2021-02-25 16:54:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ end
   def add_supply_to_winter_holidays(holiday_hash, supply)
     holiday_hash.each do |season, holiday|
       if season == :winter
-        holiday << supply
+        holiday.each {|day, items| items << supply}
       end
     end
   end
