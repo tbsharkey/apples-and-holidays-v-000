5230829547b9b3805e70de5b91ce4e924a15d827
Tom Sharkey <tbsharkey@gmail.com>
2021-02-25 17:05:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,5 +76,9 @@ end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 def all_holidays_with_bbq(holiday_hash)
-
+  holiday_hash.each do |season, holidays|
+      holidays.each do |holiday, items|
+      holiday  if items == "BBQ"
+end          
+end
 end
